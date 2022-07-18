ea23ae4986e42f245b7b427d18ea427d5c29fe23
yashinaQA42 <presnyakovae@gmail.com>
2022-07-18 14:04:33 +0300

    add text

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
-    public static void main(String[] args) {BonusMilesService service = new BonusMilesService();
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
         int price = 10_000;
         int miles = service.calculate(price);
-        System.out.println(miles);
+        System.out.println("Количество бонусных миль: " + miles);
     }
 }
